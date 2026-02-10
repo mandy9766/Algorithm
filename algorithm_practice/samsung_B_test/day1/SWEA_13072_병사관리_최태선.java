1c720cb550eac37ca466beb498dcf230e008958a
ts <mandy9766@gmail.com>
2026-02-10 17:53:17 +0900

    b형 solve

--- algorithm_practice/samsung_B_test/day1/SWEA_13072_병사관리_최태선.java
+++ algorithm_practice/samsung_B_test/day1/SWEA_13072_병사관리_최태선.java
@@ -1,35 +1,128 @@
 package algorithm_practice.samsung_B_test.day1;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
+
+
+
+
 
 class UserSolution
 {
+	public class Node{
+		int id;
+		int v;
+		Node nxt;
+
+		Node(){}
+		Node(int id,int v){
+			this.id = id;
+			this.v = v;
+			this.nxt = null;
+		}
+		Node(int id,int v,Node nxt){
+			this.id = id;
+			this.v = v;
+			this.nxt = nxt;
+		}
+	} 
+	public int [] version = new int[100001]; // 버전관리로 병사가 살아있는지 체크
+	public int [] num = new int[100001]; // id가 속한 팀 번호
+
+	public Node getNewNode(int id,Node nxt){
+		return new Node(id,++version[id],nxt);
+	}
+	public class Team{
+		Node[] head = new Node[6];
+		Node[] tail = new Node[6];
+	}
+	public Team[] t = new Team[6];
+
 	public void init()
 	{
+		for(int team = 1; team<=5;team++){
+			t[team] = new Team();
+			for(int score = 1;score<=5;score++){
+				t[team].head[score] = getNewNode(0, null);
+				t[team].tail[score] = t[team].head[score];
+			}
+		}
+		for(int id=0;id<= 100000;id++){
+			version[id] = 0;
+			num[id] = 0;
+		}
 	}
 	
 	public void hire(int mID, int mTeam, int mScore)
 	{
+		Node newNode = getNewNode(mID, null);
+		t[mTeam].tail[mScore].nxt = newNode;
+		t[mTeam].tail[mScore] = newNode;
+		num[mID] = mTeam;
 	}
 	
 	public void fire(int mID)
 	{
+		version[mID] = -1;
 	}
 
 	public void updateSoldier(int mID, int mScore)
 	{
+		hire(mID, num[mID], mScore);
 	}
 
 	public void updateTeam(int mTeam, int mChangeScore)
 	{
+		if(mChangeScore < 0){
+			for(int from= 1; from<=5;from++){
+				int to = from + mChangeScore;
+				to = (to<1)? 1: ((to>5)? 5 : to);
+				if(from == to) continue;
+
+				if(t[mTeam].head[from].nxt == null) continue;
+				t[mTeam].tail[to].nxt = t[mTeam].head[from].nxt;
+				t[mTeam].tail[to] = t[mTeam].tail[from];
+
+				t[mTeam].head[from].nxt = null;
+				t[mTeam].tail[from] = t[mTeam].head[from];
+
+			}
+		}
+		if(mChangeScore>0){
+			for (int from = 5; from >= 1; from--) {
+                int to = from + mChangeScore;
+                to = (to < 1) ? 1 : ((to > 5) ? 5 : to);
+                if (from == to) continue;
+
+                if (t[mTeam].head[from].nxt == null) continue;
+
+                t[mTeam].tail[to].nxt = t[mTeam].head[from].nxt;
+                t[mTeam].tail[to] = t[mTeam].tail[from];
+
+                t[mTeam].head[from].nxt = null;
+                t[mTeam].tail[from] = t[mTeam].head[from];
+            }
+		}
 	}
 	
 	public int bestSoldier(int mTeam)
 	{
+		for(int score = 5;score>=1;score--){
+			Node node = t[mTeam].head[score].nxt;
+			if(node == null) continue;
+
+			int bestId = 0;
+			while(node != null){
+				if(node.v == version[node.id]){
+					bestId = (bestId < node.id) ? node.id : bestId;
+				}
+				node = node.nxt;
+			}
+			if(bestId != 0) return bestId;
+		}
 		return 0;
 	}
+
 }
 
 class Solution
