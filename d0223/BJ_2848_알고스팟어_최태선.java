421db3dbdfd68af1e2f30c463e2ffc058af16483
Thomas Lee <bsjp2012@naver.com>
2026-02-24 11:34:25 +0900

    알고스팟어 solve

--- d0223/BJ_2848_알고스팟어_최태선.java
+++ d0223/BJ_2848_알고스팟어_최태선.java
@@ -8,7 +8,7 @@ public class BJ_2848_알고스팟어_최태선 {
     static String[] sArr;
     static Map<Character,List<Character>> graph;
     static Map<Character,Integer> indegree;
-    static Set<char[]> cSet;
+    static Set<String> cSet;
     static boolean isImpossible, isOnePossible;
     static StringBuilder sb;
     static int V;
@@ -23,7 +23,16 @@ public class BJ_2848_알고스팟어_최태선 {
         for(int i=0;i<N;i++){
             sArr[i] = br.readLine();
         }
-        
+        for(int i=0;i<N;i++){
+            for(int j=0;j<sArr[i].length();j++){
+                if(!graph.containsKey(sArr[i].charAt(j))){
+                    graph.put(sArr[i].charAt(j),new ArrayList<>());
+                    indegree.put(sArr[i].charAt(j),0);
+                }
+            }
+        }
+        isImpossible = false;
+
         for(int i=0;i<N;i++){
             for(int j=i+1;j<N;j++){
                 check(sArr[i],sArr[j],0);
@@ -31,11 +40,13 @@ public class BJ_2848_알고스팟어_최태선 {
         }
         V = graph.size();
 
-        isImpossible = true;
         isOnePossible = true;
         sb= new StringBuilder();
-        topologicalSrot();
-        
+
+        if (!isImpossible) {
+            topologicalSrot();
+        }
+
         if(isImpossible){
             System.out.println('!');
         }else{
@@ -62,48 +73,40 @@ public class BJ_2848_알고스팟어_최태선 {
             char c = deque.poll();
             count ++;
             sb.append(c);
-            for(char newC : graph.get(c)){
-                int val = indegree.get(newC);
-                val --;
-                if (val == 0){
-                    deque.add(newC);
-                }else{
-                    indegree.put(newC,val);
+            if (graph.get(c) != null) {
+                for(char newC : graph.get(c)){
+                    int val = indegree.get(newC);
+                    val --;
+                    indegree.put(newC, val);
+                    if (val == 0){
+                        deque.add(newC);
+                    }
                 }
             }
         }
         if(count != V)
-            isImpossible = false;
+            isImpossible = true;
 
     }
 
     static void check(String s1, String s2,int idx){ // s1은 s2보다 사전적으로 빠름
+        if (s1.length() > s2.length() && s1.startsWith(s2)) {
+            isImpossible = true;
+            return;
+        }
         if(s1.length() <= idx || s2.length() <=idx)
             return;
         char c1 = s1.charAt(idx);
         char c2 = s2.charAt(idx);
         if(c1 != c2){
-            char[] c = {c1,c2};
+            String c = c1+"-"+c2;
             if(cSet.contains(c))
                 return;
             else{
                 cSet.add(c);
-                if(graph.containsKey(c1)){
-                    graph.get(c1).add(c2);
-                }else{
-                    graph.put(c1, new ArrayList<>());
-                    graph.get(c1).add(c2);
-                }
-                if(indegree.containsKey(c2)){
-                        int val = indegree.get(c2);
-                        indegree.put(c2,val+1);
-                    }
-                else{
-                    indegree.put(c2,1);
-                }
-                if(!indegree.containsKey(c1)){
-                    indegree.put(c1,0);
-                }
+                graph.get(c1).add(c2);
+                int val = indegree.get(c2);
+                indegree.put(c2,val+1);
             }
             return;
         }
